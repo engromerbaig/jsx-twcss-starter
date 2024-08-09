4beaec29708ef506c8d5b292005151185380e1c4
Omer <engromerbaig@gmail.com>
2024-08-10 00:39:44 +0200

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+     <div className="text-red-600 text-3xl bg-green-200">
+
+      Starter set JSX + Tailwind CSS
+     </div>
+    </>
+  )
+}
+
+export default App
